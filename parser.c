dd672d2d8628e1b834fc99c45d8d8068e5500291
Ariel Don <arieldn@protonmail.com>
2021-06-24 15:52:28 -0500

    Remove carriage return from end of meta in header
    Parse Gemini header response until but not including carriage return.
    This fixes a problem with the URL parser appending the carriage return
    to its path attribute.

--- parser.c
+++ parser.c
@@ -3,8 +3,15 @@
 void
 parse_gemini_header(char *response, struct gemini_header *header)
 {
+	char *crlf = strstr(response, "\r\n");
+
 	header->status = (uint8_t)atoi(response);
 
-	strncpy(header->meta, response + 3, 1024);
+	if (crlf == NULL) {
+		strncpy(header->meta, response + 3, 1021);
+	} else {
+		strncpy(header->meta, response + 3,
+			(size_t)(crlf - response - 3));
+	}
 	header->meta[1024] = '\0';
 }
